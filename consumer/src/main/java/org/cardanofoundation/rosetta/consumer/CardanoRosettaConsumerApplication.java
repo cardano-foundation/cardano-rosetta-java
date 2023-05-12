d9e0a19dc27738506d78915dacd0f9af8950ddb7
Sotatek-LongDuong <long.duong@sotatek.com>
2023-05-12 15:46:21 +0700

    add test java

--- consumer/src/main/java/org/cardanofoundation/rosetta/consumer/CardanoRosettaConsumerApplication.java
+++ consumer/src/main/java/org/cardanofoundation/rosetta/consumer/CardanoRosettaConsumerApplication.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
