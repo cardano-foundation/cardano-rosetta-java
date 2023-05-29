268249c38992bb01ee0f2ebbac70a3744689cc73
Sotatek-LamLuong <lam.luong@sotatek.com>
2023-05-29 15:58:08 +0700

    fix: remove wrong package

--- api/src/main/java/org/cardanofoundation/rosetta/api/repository/customRepository/CustomTxMetadataRepository.java
+++
@@ -1,9 +0,0 @@
-package org.cardanofoundation.rosetta.api.repository.customRepository;
-
-import java.util.List;
-import org.cardanofoundation.rosetta.api.projection.dto.TransactionMetadataDto;
-
-public interface CustomTxMetadataRepository {
-  List<TransactionMetadataDto> findTransactionMetadata(List<String> hashes);
-
-}
