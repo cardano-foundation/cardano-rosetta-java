1ca9342c84e18a86eba67d44d2534283a29384df
Sotatek-ChienDao <chien.dao@sotatek.com>
2023-06-19 08:37:55 +0700

    test:MET-1322 update integration test for consumer

--- consumer/src/test/java/org/cardanofoundation/rosetta/consumer/kafka/KafkaProducer.java
+++ consumer/src/test/java/org/cardanofoundation/rosetta/consumer/kafka/KafkaProducer.java
@@ -20,7 +20,7 @@ public class KafkaProducer {
   private KafkaTemplate<String, Object> kafkaTemplate;
 
   public void send(String topic, CommonBlock payload) {
-    CompletableFuture<SendResult<String, Object>> future = (CompletableFuture<SendResult<String, Object>>) kafkaTemplate.send(topic,
+    CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topic,
             UUID.randomUUID().toString(), payload);
     future.whenComplete((result, ex) -> {
       if (ex != null) {
