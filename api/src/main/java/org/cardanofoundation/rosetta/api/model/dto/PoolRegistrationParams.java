1dc906297061e5fe27f32e4d88cd12e2de59dc1d
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2024-03-05 09:29:36 +0100

    Feat/construction derive (#77)
    * feat: implemented /derive

---
+++ api/src/main/java/org/cardanofoundation/rosetta/api/model/dto/PoolRegistrationParams.java
@@ -0,0 +1,28 @@
+package org.cardanofoundation.rosetta.api.model.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.cardanofoundation.rosetta.api.model.PoolMargin;
+import org.cardanofoundation.rosetta.api.model.PoolMetadata;
+import org.cardanofoundation.rosetta.api.model.Relay;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PoolRegistrationParams {
+
+    private String vrfKeyHash;
+    private String rewardAddress;
+    private String pledge;
+    private String cost;
+    private List<String> poolOwners;
+    private List<Relay> relays;
+    private PoolMargin margin;
+    private String marginPercentage;
+    private PoolMetadata poolMetadata;
+}
