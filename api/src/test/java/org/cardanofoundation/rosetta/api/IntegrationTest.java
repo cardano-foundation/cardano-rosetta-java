44b1a51a0ab4e0bd8c142c38ecd9d33709212ae0
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-03-19 15:06:19 +0100

    feat: initial implementation for a draft

--- api/src/test/java/org/cardanofoundation/rosetta/api/IntegrationTest.java
+++ api/src/test/java/org/cardanofoundation/rosetta/api/IntegrationTest.java
@@ -1,34 +1,34 @@
 package org.cardanofoundation.rosetta.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import org.cardanofoundation.rosetta.RosettaApiApplication;
-import org.junit.jupiter.api.AfterAll;
+import org.cardanofoundation.rosetta.testgenerator.common.TestConstants;
+import org.cardanofoundation.rosetta.testgenerator.common.GeneratedTestDataDTO;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 @Profile("test-integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {
     RosettaApiApplication.class})
-@Import(TestConfiguration.class)
+@Transactional
 public abstract class IntegrationTest {
 
   protected static RestTemplate restTemplate;
+  protected static GeneratedTestDataDTO generatedTestData;
 
   @LocalServerPort
   protected int serverPort;
-  protected String baseUrl = "http://localhost";
-  protected ObjectMapper objectMapper = new ObjectMapper();
 
   @BeforeAll
-  public static void init() {
+  public static void init() throws IOException {
     restTemplate = new RestTemplate();
-  }
-
-  @AfterAll
-  public static void teadDown() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    generatedTestData = objectMapper.readValue(new File("." + TestConstants.FILE_SAVE_PATH), GeneratedTestDataDTO.class);
   }
 }
