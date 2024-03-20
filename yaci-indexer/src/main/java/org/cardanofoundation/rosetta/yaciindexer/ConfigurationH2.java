ead88cae7d22eed43d34e4832b3cbf571671b3d5
shleger <sch.andrey@gmail.com>
2024-03-21 03:47:16 +0800

    feat: RA-43 h2 run OK

--- yaci-indexer/src/main/java/org/cardanofoundation/rosetta/yaciindexer/ConfigurationH2.java
+++ yaci-indexer/src/main/java/org/cardanofoundation/rosetta/yaciindexer/ConfigurationH2.java
@@ -1,18 +1,25 @@
 package org.cardanofoundation.rosetta.yaciindexer;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import java.sql.SQLException;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.h2.tools.Server;
-import java.sql.SQLException;
+
 
 @Configuration
-@ConditionalOnProperty("h2")
+@ConditionalOnClass(org.h2.tools.Server.class)
+@Slf4j
 public class ConfigurationH2 {
 
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseaServer() throws SQLException {
-        return Server.createTcpServer(
-                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-    }
+  @Bean(initMethod = "start", destroyMethod = "stop")
+  public Server inMemoryH2DatabaseaServer() throws SQLException {
+    int port = 9090;
+    log.debug("Starting H2 database server on tcp port: {}", port);
+    return Server.createTcpServer(
+        "-tcp", "-tcpAllowOthers", "-tcpPort", String.valueOf(port));
+  }
 }
--- yaci-indexer/src/main/java/org/cardanofoundation/rosetta/yaciindexer/YaciIndexerApplication.java
+++ yaci-indexer/src/main/java/org/cardanofoundation/rosetta/yaciindexer/YaciIndexerApplication.java
@@ -2,12 +2,14 @@ package org.cardanofoundation.rosetta.yaciindexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ConfigurationH2.class)
 public class YaciIndexerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(YaciIndexerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(YaciIndexerApplication.class, args);
+  }
 
 }
