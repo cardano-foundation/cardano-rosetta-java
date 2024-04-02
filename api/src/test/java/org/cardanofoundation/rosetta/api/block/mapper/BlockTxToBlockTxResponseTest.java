48720655d2ebaf6b9df262c3dc832f83220e24e4
shleger <sch.andrey@gmail.com>
2024-04-02 16:44:41 +0800

    feat: RA-44 Refactoring /block/transactions (#104)
    * feat: RA-44
    Rename domain.Transaction -> domain.Tran
    Refactor mappers
    Refactor Block/transaction controller and service
    
    * feat: RA-44  return Optional<Tran> getBlockTransaction
    
    * feat: RA-44  try fetch a transactioin
    
    * feat: RA-44  try fetch a transaction, add console.log(pm.request.json())
    
    * feat: RA-44  try fetch a transaction add first transaction hash on devkit blockchain
    
    * feat: RA-44 rm pm.request.json
    
    * feat: RA-44 hardcode blockId, blockHash, blockTransaction in /block/transaction test
    
    * feat: RA-44 add SpringMvc tests refactor integration tests
    
    * feat: RA-44 add SpringMvc get transaction test
    
    * feat: RA-44 test not found MVC scenarios
    
    * feat: RA-44 add mapper test for BlockToBlockTransactionResponse
    
    * feat: RA-44 add mapper test for BlockToBlockTransactionResponse - newUtxo
    
    * feat: RA-44 add mapper test for Entity -> Block
    
    * feat: RA-44 rename to TranToBlockTxResponse
    
    * feat: RA-44 add strict mapping strategy
    
    * feat: RA-44 reshuffle mappings
    
    * feat: RA-44 add TranToEntity mappings
    
    * feat: RA-44 refactor with model mapper
    
    * feat: RA-44 add final tests for TranToEntity mapper
    
    * feat: RA-44 add verifications
    
    * feat: RA-44 add TranToRosettaTransactionTest
    
    * feat: RA-44 add etBlockByBlockRequest unit tests
    
    * feat: RA-44 add etBlockByBlockRequest unit tests, refactor
    
    * feat: RA-44 rename Tran to BlockTx

---
+++ api/src/test/java/org/cardanofoundation/rosetta/api/block/mapper/BlockTxToBlockTxResponseTest.java
@@ -0,0 +1,153 @@
+package org.cardanofoundation.rosetta.api.block.mapper;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Set;
+
+import com.bloxbean.cardano.yaci.core.model.certs.CertificateType;
+import org.openapitools.client.model.BlockTransactionResponse;
+import org.openapitools.client.model.Relay;
+
+import org.junit.jupiter.api.Test;
+
+import org.cardanofoundation.rosetta.api.BaseMapperTest;
+import org.cardanofoundation.rosetta.api.account.model.domain.Utxo;
+import org.cardanofoundation.rosetta.api.account.model.entity.Amt;
+import org.cardanofoundation.rosetta.api.block.model.domain.Delegation;
+import org.cardanofoundation.rosetta.api.block.model.domain.PoolRegistration;
+import org.cardanofoundation.rosetta.api.block.model.domain.PoolRetirement;
+import org.cardanofoundation.rosetta.api.block.model.domain.StakeRegistration;
+import org.cardanofoundation.rosetta.api.block.model.domain.BlockTx;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BlockTxToBlockTxResponseTest extends BaseMapperTest {
+
+  @Test
+  void toDto() {
+    //given
+    TranToRosettaTransaction tx = new TranToRosettaTransaction(modelMapper);
+    TranToBlockTxResponse my = new TranToBlockTxResponse(modelMapper, tx);
+    BlockTx from = newTran();
+
+    // when
+    BlockTransactionResponse into = my.toDto(from, "5000");
+
+    // then
+    my.modelMapper.validate();
+
+    assertThat(into.getTransaction().getMetadata().getSize()).isEqualTo(from.getSize());
+    assertThat(into.getTransaction().getMetadata().getScriptSize()).isEqualTo(from.getScriptSize());
+
+    assertThat(into.getTransaction().getTransactionIdentifier().getHash())
+        .isEqualTo(from.getHash());
+
+    //TODO saa: there are no related transactions for org.openapitools.client.model.Transaction.setRelatedTransactions
+    assertThat(into.getTransaction().getRelatedTransactions()).isNull();
+
+
+  }
+
+  private BlockTx newTran() {
+    return BlockTx
+        .builder()
+        .blockNo(11L)
+        .blockHash("blockHash11")
+        .size(1L)
+        .fee("123")
+        .hash("hash12")
+        .scriptSize(0L)
+        .validContract(true)
+        .inputs(List.of(newUtxo()))
+        .delegations(newDelegations())
+        .poolRegistrations(newPoolRegistrations())
+        .poolRetirements(newPoolRetirements())
+        .stakeRegistrations(newStakeRegistrations())
+        .build();
+  }
+
+  private Utxo newUtxo() {
+    return Utxo.builder()
+        .blockHash("blockHash1")
+        .epoch(11)
+        .slot(22L)
+        .txHash("txHash1")
+        .outputIndex(44)
+        .amounts(List.of(newAmt()))
+        .dataHash("dataHash1")
+        .inlineDatum("inlineDatum1")
+        .isCollateralReturn(true)
+        .lovelaceAmount(BigInteger.TEN)
+        .ownerAddr("ownerAddr1")
+        .ownerAddrFull("ownerAddrFull1")
+        .ownerPaymentCredential("ownerPaymentCredential1")
+        .ownerStakeAddr("ownerStakeAddr1")
+        .scriptRef("scriptRef1")
+        .referenceScriptHash("referenceScriptHash1")
+        .build();
+  }
+
+  private static Amt newAmt() {
+    return Amt.builder()
+        .assetName("assetName1")
+        .policyId("policyId1")
+        .quantity(BigInteger.ONE)
+        .unit("unit1")
+        .build();
+  }
+
+  private List<StakeRegistration> newStakeRegistrations() {
+    return List.of(StakeRegistration.builder()
+        .blockHash("stakeReg_blockHash1")
+        .epoch(11)
+        .type(CertificateType.STAKE_DEREGISTRATION)
+        .slot(22L)
+        .certIndex(33L)
+        .address("stakeReg_address1")
+        .credential("stakeReg_credential1")
+        .build());
+  }
+
+  private List<PoolRetirement> newPoolRetirements() {
+    return List.of(PoolRetirement.builder()
+        .blockHash("poolRet_blockHash1")
+        .epoch(11)
+        .slot(22L)
+        .certIndex(33L)
+        .poolId("poolRet_poolId1")
+        .txHash("poolRet_txHash1")
+        .build());
+  }
+
+  private List<PoolRegistration> newPoolRegistrations() {
+    return List.of(PoolRegistration.builder()
+        .blockHash("poolReg_blockHash1")
+        .epoch(11)
+        .slot(22L)
+        .certIndex(33L)
+        .poolId("poolReg_poolId1")
+        .vrfKeyHash("poolReg_vrfKey1")
+        .pledge("poolReg_pledge1")
+        .margin("poolReg_margin1")
+        .cost("poolReg_cost1")
+        .rewardAccount("poolReg_rewardAccount1")
+        .owners(Set.of("poolReg_owner1"))
+        .relays(List.of(Relay.builder().ipv4("poolReg_ipv4_1").
+            ipv6("poolReg_ipv6_1").port(123).dnsName("dnsName1").build()))
+        .build());
+  }
+
+  private List<Delegation> newDelegations() {
+    return List.of(Delegation.builder()
+        .blockHash("delegation_blockHash1")
+        .epoch(11)
+        .slot(22L)
+        .certIndex(33L)
+        .poolId("delegation_poolId1")
+        .credential("delegation_account1")
+        .txHash("delegation_txHash1")
+        .address("delegation_address1")
+        .build());
+  }
+
+}
