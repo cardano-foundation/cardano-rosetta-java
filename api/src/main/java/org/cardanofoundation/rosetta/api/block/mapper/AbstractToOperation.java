a72303f2c9862eacf1f9265e9ab0009b7a97d8b4
shleger <sch.andrey@gmail.com>
2024-04-08 01:57:10 +0800

    feat: RA-44 OutputToOperation OK

--- api/src/main/java/org/cardanofoundation/rosetta/api/block/mapper/AbstractToOperation.java
+++ api/src/main/java/org/cardanofoundation/rosetta/api/block/mapper/AbstractToOperation.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.mutable.MutableInt;
+import org.jetbrains.annotations.NotNull;
 import org.openapitools.client.model.Amount;
 import org.openapitools.client.model.Currency;
 import org.openapitools.client.model.Operation;
@@ -17,7 +18,7 @@ import org.cardanofoundation.rosetta.api.account.model.entity.Amt;
 import org.cardanofoundation.rosetta.common.mapper.DataMapper;
 import org.cardanofoundation.rosetta.common.util.Constants;
 
-public abstract class  AbstractToOperation<T> {
+public abstract class AbstractToOperation<T> {
 
   abstract Operation toDto(T model, OperationStatus status, int index);
 
@@ -30,24 +31,36 @@ public abstract class  AbstractToOperation<T> {
         .collect(Collectors.toList());
   }
 
-  protected static  OperationMetadata mapToOperationMetaData(boolean spent, List<Amt> amounts) {
+  protected static OperationMetadata mapToOperationMetaData(boolean spent, List<Amt> amounts) {
     OperationMetadata operationMetadata = new OperationMetadata();
-    for (Amt amount : amounts) {
-      if (!amount.getAssetName().equals(Constants.LOVELACE)) {
-        TokenBundleItem tokenBundleItem = new TokenBundleItem();
-        tokenBundleItem.setPolicyId(amount.getPolicyId());
-        Amount amt = new Amount();
-        amt.setValue(DataMapper.mapValue(amount.getQuantity().toString(), spent));
-        String hexAssetName = Hex.encodeHexString(amount.getAssetName().getBytes());
-        amt.setCurrency(Currency.builder()
-            .symbol(hexAssetName)
-            .decimals(0)
-            .build());
-        tokenBundleItem.setTokens(List.of(amt));
-        operationMetadata.addTokenBundleItem(tokenBundleItem);
-      }
-    }
+    Optional.ofNullable(amounts)
+        .stream()
+        .flatMap(List::stream)
+        .forEach(amount -> {
+          operationMetadata.setDepositAmount(getDepositAmount());
+          if (!amount.getAssetName().equals(Constants.LOVELACE)) {
+            TokenBundleItem tokenBundleItem = new TokenBundleItem();
+            tokenBundleItem.setPolicyId(amount.getPolicyId());
+            Amount amt = new Amount();
+            amt.setValue(DataMapper.mapValue(amount.getQuantity().toString(), spent));
+            String hexAssetName = Hex.encodeHexString(amount.getAssetName().getBytes());
+            amt.setCurrency(Currency.builder()
+                .symbol(hexAssetName)
+                .decimals(0)
+                .build());
+            tokenBundleItem.setTokens(List.of(amt));
+            operationMetadata.addTokenBundleItem(tokenBundleItem);
+          }
+        });
     return operationMetadata;
+  }
+
+  // TODO saa: need to get this '"2000000", Constants.ADA, Constants.ADA_DECIMALS,' from protocolparams
+  // Create and inject  GenesisService to get the stake deposit amount
+  // see similar implementation in BlockService.getPoolDeposit
+  @NotNull
+  private static Amount getDepositAmount() {
+    return DataMapper.mapAmount("2000000", Constants.ADA, Constants.ADA_DECIMALS, null);
   }
 
 }
--- api/src/main/java/org/cardanofoundation/rosetta/api/block/mapper/OutputToOperation.java
+++ api/src/main/java/org/cardanofoundation/rosetta/api/block/mapper/OutputToOperation.java
@@ -4,17 +4,14 @@ import java.util.Optional;
 
 import lombok.AllArgsConstructor;
 
-import com.bloxbean.cardano.yaci.core.model.certs.CertificateType;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.openapitools.client.model.CoinAction;
 import org.openapitools.client.model.Operation;
-import org.openapitools.client.model.OperationMetadata;
 import org.openapitools.client.model.OperationStatus;
 
 import org.cardanofoundation.rosetta.api.account.model.domain.Utxo;
 import org.cardanofoundation.rosetta.common.annotation.OpenApiMapper;
-import org.cardanofoundation.rosetta.common.enumeration.OperationType;
-import org.cardanofoundation.rosetta.common.mapper.DataMapper;
 import org.cardanofoundation.rosetta.common.util.Constants;
 
 import static org.cardanofoundation.rosetta.common.util.Constants.ADA;
@@ -32,7 +29,6 @@ public class OutputToOperation extends AbstractToOperation<Utxo> {
         .ofNullable(modelMapper.getTypeMap(Utxo.class, Operation.class))
         .orElseGet(() -> modelMapper.createTypeMap(Utxo.class, Operation.class))
         .addMappings(mp -> {
-
           mp.map(f -> Constants.OUTPUT, Operation::setType);
           mp.map(f -> status.getStatus(), Operation::setStatus);
           mp.<Long>map(f -> index, (d, v) -> d.getOperationIdentifier().setIndex(v));
@@ -43,34 +39,11 @@ public class OutputToOperation extends AbstractToOperation<Utxo> {
           mp.<String>map(f -> model.getTxHash() + ":" + model.getOutputIndex(),
               (d, v) -> d.getCoinChange().getCoinIdentifier().setIdentifier(v));
           mp.<CoinAction>map(f -> CoinAction.CREATED, (d, v) -> d.getCoinChange().setCoinAction(v));
-//          mp.map(f -> mapToOperationMetaData(false, f.getAmounts()), Operation::setMetadata);
+          mp.map(f-> mapToOperationMetaData(false, model.getAmounts()), Operation::setMetadata);
 
         })
-        .setPostConverter(ctx -> {
-
-          ctx.getDestination().setStatus(status.getStatus());
-          ctx.getDestination().setMetadata(OperationMetadata.builder()
-              .depositAmount(DataMapper.mapAmount("2000000", ADA, ADA_DECIMALS, null))
-              .build());
-          // TODO saa: need to get this from protocolparams
-          // Create and inject  GenesisService to get the stake deposit amount
-          // see similar implementation in BlockService.getPoolDeposit
-
-          return ctx.getDestination();
-        }).map(model);
+        //strange but without it method mapToOperationMetaData in above mapper did not work
+        .setPostConverter(MappingContext::getDestination)
+        .map(model);
   }
-
-
-  private String convert(CertificateType model) {
-    if (model == null) {
-      return null;
-    } else {
-      return model.equals(CertificateType.STAKE_REGISTRATION)
-          ? OperationType.STAKE_KEY_REGISTRATION.toString() :
-          model.equals(CertificateType.STAKE_DEREGISTRATION)
-              ? OperationType.STAKE_KEY_DEREGISTRATION.toString() : null;
-    }
-  }
-
-
 }
