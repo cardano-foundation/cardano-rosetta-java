032881d288bf31cf1cf5e07f869d91bfe48a4923
Dmitrii <dmitrii_berezin@epam.com>
2024-04-26 23:50:35 +0300

    fix: Fixing /construction/parse error handling

--- api/src/main/java/org/cardanofoundation/rosetta/common/mapper/CborArrayToTransactionData.java
+++ api/src/main/java/org/cardanofoundation/rosetta/common/mapper/CborArrayToTransactionData.java
@@ -6,11 +6,14 @@ import co.nstant.in.cbor.model.Array;
 import co.nstant.in.cbor.model.DataItem;
 import co.nstant.in.cbor.model.Map;
 import co.nstant.in.cbor.model.UnicodeString;
+import com.bloxbean.cardano.client.common.cbor.CborSerializationUtil;
 import com.bloxbean.cardano.client.exception.CborDeserializationException;
 import com.bloxbean.cardano.client.transaction.spec.Transaction;
 import com.bloxbean.cardano.client.transaction.spec.TransactionBody;
 import com.bloxbean.cardano.client.util.HexUtil;
 import java.util.List;
+
+import org.cardanofoundation.rosetta.common.exception.ExceptionFactory;
 import org.cardanofoundation.rosetta.common.model.cardano.transaction.TransactionData;
 import org.cardanofoundation.rosetta.common.model.cardano.transaction.TransactionExtraData;
 
@@ -26,18 +29,27 @@ public class CborArrayToTransactionData {
     TransactionBody body;
     if (signed) {
       List<DataItem> decode = CborDecoder.decode(bytes);
-      Array array = (Array) decode.get(0);
-      if (decode.size() >= 2 && array.getDataItems().size() == 3) {
-        array.add(decode.get(1));
+      if (decode.get(0) instanceof Array array) {
+        if (decode.size() >= 2 && array.getDataItems().size() == 3) {
+          array.add(decode.get(1));
+        }
+        Transaction parsed = Transaction.deserialize(
+            com.bloxbean.cardano.yaci.core.util.CborSerializationUtil.serialize(array));
+        body = parsed.getBody();
+      } else {
+        throw ExceptionFactory.cantCreateSignedTransactionFromBytes();
       }
-      Transaction parsed = Transaction.deserialize(
-          com.bloxbean.cardano.yaci.core.util.CborSerializationUtil.serialize(array));
-      body = parsed.getBody();
     } else {
-      body = TransactionBody.deserialize(
-          (Map) com.bloxbean.cardano.client.common.cbor.CborSerializationUtil.deserialize(bytes));
+      DataItem deserializedTrBody = CborSerializationUtil.deserialize(bytes);
+      if (deserializedTrBody instanceof Map deserializedMap) {
+        body = TransactionBody.deserialize(deserializedMap);
+      } else {
+        throw ExceptionFactory.cantCreateUnsignedTransactionFromBytes();
+      }
     }
     return new TransactionData(body, extraData);
   }
 
+  private CborArrayToTransactionData() {
+  }
 }
