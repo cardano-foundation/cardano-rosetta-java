91e535e3f74db706037374555b418c085aeb40f1
Berezin Dmitrii <dmitrii_berezin@epam.com>
2024-05-02 12:30:04 +0300

    Refactoring: Spotless applied fix

--- api/src/main/java/org/cardanofoundation/rosetta/common/services/impl/PostgresLedgerDataProviderService.java
+++ api/src/main/java/org/cardanofoundation/rosetta/common/services/impl/PostgresLedgerDataProviderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
 import org.modelmapper.ModelMapper;
 import org.openapitools.client.model.Currency;
