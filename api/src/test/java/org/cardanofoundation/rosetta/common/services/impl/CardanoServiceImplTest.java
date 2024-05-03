f9dc69576db88c1466b5ee8c19182cfd6089c13b
shleger <sch.andrey@gmail.com>
2024-05-03 17:44:25 +0800

    fix: RA-90 fix spotless

--- api/src/test/java/org/cardanofoundation/rosetta/common/services/impl/CardanoServiceImplTest.java
+++ api/src/test/java/org/cardanofoundation/rosetta/common/services/impl/CardanoServiceImplTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import jakarta.validation.constraints.NotNull;
 
 import org.springframework.http.HttpEntity;
