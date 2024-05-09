a33fe8014e98548ea208b88a5b7db746fd35f7ca
shleger <sch.andrey@gmail.com>
2024-05-09 15:50:55 +0800

    refactor: RA-107 refactor accBalanceTest

--- api/src/test/java/org/cardanofoundation/rosetta/api/SpringMvcTest.java
+++ api/src/test/java/org/cardanofoundation/rosetta/api/SpringMvcTest.java
@@ -1,10 +1,20 @@
 package org.cardanofoundation.rosetta.api;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import org.cardanofoundation.rosetta.testgenerator.common.TestConstants;
+import org.cardanofoundation.rosetta.testgenerator.common.TransactionBlockDetails;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,5 +25,19 @@ public class SpringMvcTest {
 
   @Autowired
   protected MockMvc mockMvc;
+
+  protected static Map<String, TransactionBlockDetails> generatedDataMap;
+
+
+  @Deprecated
+  protected int serverPort;
+
+  @BeforeAll
+  public static void init() throws IOException {
+    generatedDataMap = new ObjectMapper().readValue(
+        new File("." + TestConstants.FILE_SAVE_PATH),
+        new TypeReference<>() {
+        });
+  }
 
 }
