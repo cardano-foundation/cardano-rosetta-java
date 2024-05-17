6c3b936500ee7634409b93314b7adbd45f650d94
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-05-17 08:54:46 +0200

    fix: reverting the mappers. They will be optimized in a different branch

--- api/src/main/java/org/cardanofoundation/rosetta/common/mapper/ProtocolParamsToEntity.java
+++ api/src/main/java/org/cardanofoundation/rosetta/common/mapper/ProtocolParamsToEntity.java
@@ -6,7 +6,6 @@ import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 
 import org.cardanofoundation.rosetta.api.block.model.domain.ProtocolParams;
-import org.cardanofoundation.rosetta.api.block.model.domain.ProtocolParams.ProtocolVersion;
 import org.cardanofoundation.rosetta.api.block.model.entity.ProtocolParamsEntity;
 import org.cardanofoundation.rosetta.common.annotation.PersistenceMapper;
 
@@ -16,34 +15,17 @@ public class ProtocolParamsToEntity {
 
   final ModelMapper modelMapper;
 
-  // TODO need to be removed - These mapper slow down our response times
-//  public ProtocolParams fromEntity(ProtocolParamsEntity entity){
-//    return modelMapper.typeMap(ProtocolParamsEntity.class, ProtocolParams.class)
-//        .implicitMappings()
-//        .addMappings(mapper -> {
-//          mapper.<Integer>map(ProtocolParamsEntity::getProtocolMajorVer, (dest, v) -> dest.getProtocolVersion().setMajor(v));
-//          mapper.<Integer>map(ProtocolParamsEntity::getProtocolMinorVer, (dest, v) -> dest.getProtocolVersion().setMinor(v));
-//        })
-//        .map(entity);
-//  }
-
-  public ProtocolParams fromEntity(ProtocolParamsEntity entity) {
-    ProtocolVersion protocolVersion = new ProtocolVersion();
-    protocolVersion.setMajor(entity.getProtocolMajorVer());
-    protocolVersion.setMinor(entity.getProtocolMinorVer());
-    return ProtocolParams.builder()
-        .minFeeA(entity.getMinFeeA())
-        .minFeeB(entity.getMinFeeB())
-        .maxTxSize(entity.getMaxTxSize())
-        .keyDeposit(entity.getKeyDeposit())
-        .poolDeposit(entity.getPoolDeposit())
-        .protocolVersion(protocolVersion)
-        .minPoolCost(entity.getMinPoolCost())
-        .adaPerUtxoByte(entity.getAdaPerUtxoByte())
-        .costModels(entity.getCostModels())
-        .maxValSize(entity.getMaxValSize())
-        .maxCollateralInputs(entity.getMaxCollateralInputs())
-        .build();
+  public ProtocolParams fromEntity(ProtocolParamsEntity entity){
+    return modelMapper.typeMap(ProtocolParamsEntity.class, ProtocolParams.class)
+        .implicitMappings()
+        .addMappings(mapper -> {
+          mapper.<String>map(ProtocolParamsEntity::getExtraEntropy, (dest, v) -> dest.getExtraEntropy().setTag(v));
+          mapper.map(ProtocolParamsEntity::getMaxBlockSize, ProtocolParams::setMaxBlockBodySize);
+          mapper.<Integer>map(ProtocolParamsEntity::getProtocolMajorVer, (dest, v) -> dest.getProtocolVersion().setMajor(v));
+          mapper.<Integer>map(ProtocolParamsEntity::getProtocolMinorVer, (dest, v) -> dest.getProtocolVersion().setMinor(v));
+          mapper.map(ProtocolParamsEntity::getMinUtxo, ProtocolParams::setMinUtxoValue);
+        })
+        .map(entity);
   }
 
   public ProtocolParams merge(ProtocolParams from, ProtocolParams to){
--- api/src/main/java/org/cardanofoundation/rosetta/common/mapper/ProtocolParamsToRosettaProtocolParameters.java
+++ api/src/main/java/org/cardanofoundation/rosetta/common/mapper/ProtocolParamsToRosettaProtocolParameters.java
@@ -14,33 +14,17 @@ public class ProtocolParamsToRosettaProtocolParameters {
 
   final ModelMapper modelMapper;
 
-  // TODO need to be removed, since it's much slower -> 18ms
-//  public ProtocolParameters toProtocolParameters(ProtocolParams model) {
-//    return modelMapper.typeMap(ProtocolParams.class, ProtocolParameters.class)
-//        .addMappings(mapper -> {
-//          mapper.map(ProtocolParams::getAdaPerUtxoByte, ProtocolParameters::setCoinsPerUtxoSize);
-//          mapper.map(ProtocolParams::getMaxCollateralInputs,
-//              ProtocolParameters::setMaxCollateralInputs);
-//          mapper.map(ProtocolParams::getMinFeeA, ProtocolParameters::setMinFeeCoefficient);
-//          mapper.map(ProtocolParams::getMinFeeB, ProtocolParameters::setMinFeeConstant);
-//          mapper.map(source -> source.getProtocolVersion().getMajor(),
-//              ProtocolParameters::setProtocol);
-//        })
-//        .map(model);
-//  }
-
   public ProtocolParameters toProtocolParameters(ProtocolParams model) {
-    return ProtocolParameters.builder()
-        .coinsPerUtxoSize(model.getAdaPerUtxoByte().toString())
-        .maxTxSize(model.getMaxTxSize())
-        .maxValSize(model.getMaxValSize())
-        .keyDeposit(model.getKeyDeposit().toString())
-        .maxCollateralInputs(model.getMaxCollateralInputs())
-        .minFeeCoefficient(model.getMinFeeA())
-        .minFeeConstant(model.getMinFeeB())
-        .poolDeposit(model.getPoolDeposit().toString())
-        .minPoolCost(model.getMinPoolCost().toString())
-        .protocol(model.getProtocolVersion().getMajor())
-        .build();
+    return modelMapper.typeMap(ProtocolParams.class, ProtocolParameters.class)
+        .addMappings(mapper -> {
+          mapper.map(ProtocolParams::getAdaPerUtxoByte, ProtocolParameters::setCoinsPerUtxoSize);
+          mapper.map(ProtocolParams::getMaxCollateralInputs,
+              ProtocolParameters::setMaxCollateralInputs);
+          mapper.map(ProtocolParams::getMinFeeA, ProtocolParameters::setMinFeeCoefficient);
+          mapper.map(ProtocolParams::getMinFeeB, ProtocolParameters::setMinFeeConstant);
+          mapper.map(source -> source.getProtocolVersion().getMajor(),
+              ProtocolParameters::setProtocol);
+        })
+        .map(model);
   }
 }
