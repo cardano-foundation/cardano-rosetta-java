06e757c9a1575644ab16f531bf6413e232e39c05
Mikhail_Linnik1 <mikhail_linnik1@epam.com>
2024-05-20 20:13:55 +0200

    RA-142 Tests added

---
+++ api/src/test/java/org/cardanofoundation/rosetta/api/block/mapper/AbstractToOperationTest.java
@@ -0,0 +1,103 @@
+package org.cardanofoundation.rosetta.api.block.mapper;
+
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
+import org.mockito.Mockito;
+import org.openapitools.client.model.Amount;
+import org.openapitools.client.model.Currency;
+import org.openapitools.client.model.OperationMetadata;
+import org.openapitools.client.model.TokenBundleItem;
+
+import org.junit.jupiter.api.Test;
+
+import org.cardanofoundation.rosetta.api.account.model.domain.Amt;
+import org.cardanofoundation.rosetta.common.util.Constants;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class AbstractToOperationTest {
+
+  final private AbstractToOperation abstractToOperation = Mockito.mock(
+      AbstractToOperation.class,
+      Mockito.CALLS_REAL_METHODS);
+
+  @Test
+  public void mapToOperationMetaDataTest() {
+    // given
+    List<Amt> amtList = Arrays.asList(
+        newAmt(1, 11, true),
+        newAmt(1, 12, false),
+        newAmt(1, 13, false),
+        newAmt(1, 14, false),
+        newAmt(2, 21, false),
+        newAmt(2, 22, true),
+        newAmt(3, 31, false),
+        newAmt(3, 32, true),
+        newAmt(3, 33, false),
+        newAmt(4, 41, true)
+    );
+    // when
+    OperationMetadata operationMetadata = abstractToOperation.mapToOperationMetaData(true, amtList);
+    // then
+    assertNotNull(operationMetadata);
+    List<TokenBundleItem> tokenBundle = operationMetadata.getTokenBundle();
+    assertEquals(3, tokenBundle.size());
+
+    List<Amount> policyId1 = tokenBundle.stream()
+        .filter(t -> t.getPolicyId().equals("policyId1"))
+        .map(TokenBundleItem::getTokens)
+        .flatMap(List::stream)
+        .toList();
+
+    Assertions.assertThat(getPolicyIdUnits(tokenBundle, "policyId1"))
+        .containsExactlyInAnyOrder("unit12", "unit13", "unit14");
+
+    Assertions.assertThat(getPolicyIdUnits(tokenBundle, "policyId2"))
+        .containsExactlyInAnyOrder("unit21");
+
+    Assertions.assertThat(getPolicyIdUnits(tokenBundle, "policyId3"))
+        .containsExactlyInAnyOrder("unit31", "unit33");
+  }
+
+  @Test
+  public void mapToOperationMetaDataNegativeTest() {
+    // given
+    List<Amt> amtList = Arrays.asList(
+        newAmt(1, 11, true),
+        newAmt(2, 21, true));
+    // when
+    OperationMetadata operationMetadata = abstractToOperation.mapToOperationMetaData(true, amtList);
+    // then
+    assertNull(operationMetadata);
+  }
+
+  private static List<String> getPolicyIdUnits(List<TokenBundleItem> tokenBundle, String policyId) {
+    return tokenBundle.stream()
+        .filter(t -> t.getPolicyId().equals(policyId))
+        .map(TokenBundleItem::getTokens)
+        .flatMap(List::stream)
+        .map(Amount::getCurrency)
+        .map(Currency::getSymbol)
+        .toList();
+  }
+
+  private static Amt newAmt(int policy, int number, boolean isLovelace) {
+    return Amt.builder()
+        .assetName(isLovelace ? Constants.LOVELACE : "assetName" + number)
+        .policyId("policyId" + policy)
+        .quantity(BigInteger.ONE)
+        .unit("unit" + number)
+        .build();
+  }
+
+}
+
+
+
