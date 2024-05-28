38245ebd7c4131ea886c178587e09b39a5443141
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-05-28 16:01:18 +0200

    refactor: refactored CustomTransactionSizeStore, added spotless

--- api/src/main/java/org/cardanofoundation/rosetta/api/block/model/entity/TransactionSizeEntity.java
+++ api/src/main/java/org/cardanofoundation/rosetta/api/block/model/entity/TransactionSizeEntity.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
