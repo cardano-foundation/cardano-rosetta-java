1f1173acef09af2fd7660085c35903c64bb05165
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-05-28 09:01:01 +0200

    chore: testing gha

--- api/src/main/java/org/cardanofoundation/rosetta/api/account/service/LedgerAccountServiceImpl.java
+++ api/src/main/java/org/cardanofoundation/rosetta/api/account/service/LedgerAccountServiceImpl.java
@@ -48,41 +48,24 @@ public class LedgerAccountServiceImpl implements LedgerAccountService {
 
   private static List<AddressBalance> groupAddressBalancesBasedOnUnit(
       List<AddressUtxoEntity> unspendUtxosByAddressAndBlock) {
-//    Map<String, AddressBalance> map = new HashMap<>();
-//    unspendUtxosByAddressAndBlock.forEach(entity -> entity.getAmounts().forEach(amt -> {
-//      BigInteger quantity = amt.getQuantity();
-//      if(map.containsKey(amt.getUnit())) {
-//        quantity = quantity.add(map.get(amt.getUnit()).quantity());
-//      }
-//      map.put(amt.getUnit(), AddressBalance.builder()
-//          .address(entity.getOwnerAddr())
-//          .unit(amt.getUnit())
-//          .quantity(quantity)
-//          .number(entity.getBlockNumber())
-//          .build());
-//
-//    }));
-//    return map.values().stream().toList();
     List<AddressBalance> retList = new ArrayList<>();
-    unspendUtxosByAddressAndBlock.forEach(entity -> {
-      entity.getAmounts().forEach(amt -> {
-        Optional<AddressBalance> first = retList.stream()
-            .filter(addressBalance -> addressBalance.unit().equals(amt.getUnit())).findFirst();
-        BigInteger quantity;
-        if(first.isPresent()) {
-          quantity = first.get().quantity().add(amt.getQuantity());
-          retList.remove(first.get());
-        } else {
-          quantity = amt.getQuantity();
-        }
-        retList.add(AddressBalance.builder()
-                .unit(amt.getUnit())
-                .address(entity.getOwnerAddr())
-                .quantity(quantity)
-                .number(entity.getBlockNumber())
-            .build());
-      });
-    });
+    unspendUtxosByAddressAndBlock.forEach(entity -> entity.getAmounts().forEach(amt -> {
+      Optional<AddressBalance> first = retList.stream()
+          .filter(addressBalance -> addressBalance.unit().equals(amt.getUnit())).findFirst();
+      BigInteger quantity;
+      if(first.isPresent()) {
+        quantity = first.get().quantity().add(amt.getQuantity());
+        retList.remove(first.get());
+      } else {
+        quantity = amt.getQuantity();
+      }
+      retList.add(AddressBalance.builder()
+              .unit(amt.getUnit())
+              .address(entity.getOwnerAddr())
+              .quantity(quantity)
+              .number(entity.getBlockNumber())
+          .build());
+    }));
     return retList;
   }
 
