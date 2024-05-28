ac143e2b9aba1bd7639aaf203121e04368e2efa2
Berezin Dmitrii <dmitrii_berezin@epam.com>
2024-05-28 17:29:13 +0300

    Fix: Spotless fix

--- api/src/test/java/org/cardanofoundation/rosetta/api/account/service/AccountServiceImplTest.java
+++ api/src/test/java/org/cardanofoundation/rosetta/api/account/service/AccountServiceImplTest.java
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.cardanofoundation.rosetta.api.account.model.domain.AddressBalance;
 import org.cardanofoundation.rosetta.api.account.model.domain.Amt;
 import org.cardanofoundation.rosetta.api.account.model.domain.Utxo;
-import org.cardanofoundation.rosetta.api.block.model.domain.Block;
 import org.cardanofoundation.rosetta.api.block.model.domain.BlockIdentifierExtended;
 import org.cardanofoundation.rosetta.api.block.service.LedgerBlockService;
 import org.cardanofoundation.rosetta.common.exception.ApiException;
