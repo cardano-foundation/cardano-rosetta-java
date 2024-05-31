8c0035d29a0823b8a02e4b7a91fe44308459675b
Gennadii Tumskii <tumskiy78@gmail.com>
2024-05-31 14:11:28 +0200

    RA-146: Minor changes

--- api/src/main/java/org/cardanofoundation/rosetta/config/CacheConfig.java
+++ api/src/main/java/org/cardanofoundation/rosetta/config/CacheConfig.java
@@ -4,12 +4,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableCaching
-@EnableScheduling
 public class CacheConfig {
+
   @Bean
   public ConcurrentMapCacheManager cacheManager() {
     return new ConcurrentMapCacheManager("protocolParamsCache");
