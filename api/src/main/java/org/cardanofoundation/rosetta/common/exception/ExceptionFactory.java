014a31a126bb0aa729d33f80b15e7b0b6287c418
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2025-03-21 16:15:26 +0100

    refactor: fail faster and less code complexity due to structured concurrency.

--- api/src/main/java/org/cardanofoundation/rosetta/common/exception/ExceptionFactory.java
+++ api/src/main/java/org/cardanofoundation/rosetta/common/exception/ExceptionFactory.java
@@ -317,7 +317,7 @@ public class ExceptionFactory {
   }
 
   public static ApiException misconfiguredTime(LocalDateTime now) {
-    return new ApiException(RosettaErrorType.MISCONFIGURED_TIME.toRosettaError(false, Details.builder().message("Current time: " + now).build()));
+    return new ApiException(RosettaErrorType.MISCONFIGURED_TIME.toRosettaError(false, Details.builder().message(STR."Current time: \{now}").build()));
   }
 
   public static ApiException missingDRepId() {
@@ -326,6 +326,10 @@ public class ExceptionFactory {
 
   public static ApiException missingDrep() {
     return new ApiException(RosettaErrorType.MISSING_DREP_TYPE.toRosettaError(false));
+  }
+
+  public static ApiException timeOut(String detailMessage) {
+    return new ApiException(RosettaErrorType.TIMEOUT.toRosettaError(true, Details.builder().message(STR."Timeout, details: \{detailMessage}").build()));
   }
 
 }
