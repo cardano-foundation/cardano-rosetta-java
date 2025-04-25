152ac81c7506b3df6c693d299db27a5cb92e775f
Lincon Vidal <lincon.vidal@cardanofoundation.org>
2025-04-25 16:38:33 -0300

    chore: update terminology and enhance feature descriptions for clarity

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cardano Rosetta Java',
-  tagline: 'Java implementation of the Rosetta API for Cardano',
+  tagline: 'A lightweight Java implementation of the Mesh (formerly Rosetta) API for Cardano',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
