bb6b3c9318080437c6eef791b0a92c97655b1130
VladislavKudrin <vladislavkudrin@gmail.com>
2025-05-06 10:40:05 +0200

    fix: postgres missing port and cnode socket path (#442)

--- docker/dockerfiles/node/entrypoint.sh
+++ docker/dockerfiles/node/entrypoint.sh
@@ -17,6 +17,9 @@ trap clean_up SIGHUP SIGINT SIGTERM
 
 echo "Starting Cardano node..."
 
+
+mkdir -p "$(dirname "$CARDANO_NODE_SOCKET_PATH")"
+sleep 1
 cardano-node run --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $CARDANO_NODE_PORT --database-path /node/db --config /config/config.json --topology /config/topology.json > /logs/node.log &
 CARDANO_NODE_PID=$!
 sleep 2
