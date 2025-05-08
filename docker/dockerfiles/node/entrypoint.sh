fa165167300b13b4b7d07022709b7328c833711c
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2025-05-08 09:20:12 +0200

    fix: do not redirect logs to a file but to sysout.

--- docker/dockerfiles/node/entrypoint.sh
+++ docker/dockerfiles/node/entrypoint.sh
@@ -20,12 +20,12 @@ echo "Starting Cardano node..."
 
 mkdir -p "$(dirname "$CARDANO_NODE_SOCKET_PATH")"
 sleep 1
-cardano-node run --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $CARDANO_NODE_PORT --database-path /node/db --config /config/config.json --topology /config/topology.json > /logs/node.log &
+cardano-node run --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $CARDANO_NODE_PORT --database-path /node/db --config /config/config.json --topology /config/topology.json &
 CARDANO_NODE_PID=$!
 sleep 2
 
 echo "Starting Cardano submit api..."
-cardano-submit-api --listen-address 0.0.0.0 --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $NODE_SUBMIT_API_PORT $NETWORK_STR  --config /cardano-submit-api-config/cardano-submit-api.yaml > /logs/submit-api.log &
+cardano-submit-api --listen-address 0.0.0.0 --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $NODE_SUBMIT_API_PORT $NETWORK_STR  --config /cardano-submit-api-config/cardano-submit-api.yaml &
 CARDANO_SUBMIT_PID=$!
 
 wait -n $CARDANO_NODE_PID $CARDANO_SUBMIT_PID
