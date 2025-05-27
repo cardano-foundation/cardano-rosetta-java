0bc8fbf117846054aa2878b6844108be7f091a14
VladislavKudrin <vladislavkudrin@gmail.com>
2025-05-27 12:38:40 +0200

    feat: split cardano-node and cardano-submit-api (#464)

--- docker/dockerfiles/node/entrypoint.sh
+++ docker/dockerfiles/node/entrypoint.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 if [ "$NETWORK" == "mainnet" ]; then
-    NETWORK_STR="--mainnet"
+  NETWORK_STR="--mainnet"
 else
-    NETWORK_STR="--testnet-magic $PROTOCOL_MAGIC"
+  NETWORK_STR="--testnet-magic $PROTOCOL_MAGIC"
 fi
 
-function clean_up() {
-  # Killing all processes before exiting
-  kill -2 "$CARDANO_NODE_PID" "$CARDANO_SUBMIT_PID" 
-  wait $CARDANO_NODE_PID
-  exit
-}
-
-trap clean_up SIGHUP SIGINT SIGTERM
-
-echo "Starting Cardano node..."
-cardano-node run --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $CARDANO_NODE_PORT --database-path "$CARDANO_NODE_DB" --config /config/config.json --topology /config/topology.json &
-CARDANO_NODE_PID=$!
-sleep 2
-
-echo "Starting Cardano submit api..."
-cardano-submit-api --listen-address 0.0.0.0 --socket-path "$CARDANO_NODE_SOCKET_PATH" --port $NODE_SUBMIT_API_PORT $NETWORK_STR  --config /cardano-submit-api-config/cardano-submit-api.yaml &
-CARDANO_SUBMIT_PID=$!
-
-wait -n $CARDANO_NODE_PID $CARDANO_SUBMIT_PID
+cmd="$1"; shift
+case "$cmd" in
+  cardano-node)
+    echo "Starting Cardano node..."
+    exec cardano-node run \
+      --socket-path "$CARDANO_NODE_SOCKET_PATH" \
+      --port "$CARDANO_NODE_PORT" \
+      --database-path "$CARDANO_NODE_DB" \
+      --config /config/config.json \
+      --topology /config/topology.json
+    ;;
+  cardano-submit-api)
+    echo "Starting Cardano submit api..."
+    exec cardano-submit-api \
+      --listen-address 0.0.0.0 \
+      --socket-path "$CARDANO_NODE_SOCKET_PATH" \
+      --port "$NODE_SUBMIT_API_PORT" \
+      $NETWORK_STR \
+      --config /cardano-submit-api-config/cardano-submit-api.yaml
+    ;;
+  *)
+    exit 1
+    ;;
+esac
