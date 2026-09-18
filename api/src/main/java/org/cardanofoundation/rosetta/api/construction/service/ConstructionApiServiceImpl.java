efb326b92f97ee2d821b59b8ba56afd42682e00e
Kartiiyer12 <kartikiyer2020@gmail.com>
2026-09-18 14:59:30 +0100

    style: flatten CIP-113 staking validation

--- api/src/main/java/org/cardanofoundation/rosetta/api/construction/service/ConstructionApiServiceImpl.java
+++ api/src/main/java/org/cardanofoundation/rosetta/api/construction/service/ConstructionApiServiceImpl.java
@@ -79,10 +79,9 @@ public class ConstructionApiServiceImpl implements ConstructionApiService {
     if (addressType == AddressType.BASE) {
       stakingCredential = Optional.ofNullable(metadata.getStakingCredential())
               .orElseThrow(ExceptionFactory::missingStakingKeyError);
-    } else if (addressType == AddressType.CIP_113) {
-      if (metadata.getStakingCredential() != null) {
-        throw ExceptionFactory.cip113StakingCredentialNotAllowed();
-      }
+    } else if (addressType == AddressType.CIP_113
+            && metadata.getStakingCredential() != null) {
+      throw ExceptionFactory.cip113StakingCredentialNotAllowed();
     }
     String address = cardanoConstructionService
             .getCardanoAddress(addressType, stakingCredential, publicKey, networkEnum);
